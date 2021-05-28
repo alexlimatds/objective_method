1eb2214d73d682093cc293ba0bb8d872d05660c1
Alexandre Lima <alexlimatds@yahoo.com.br>
2021-05-28 10:08:00 -0300

    Documenting the code

--- terms_categorization.py
+++ terms_categorization.py
@@ -8,26 +8,52 @@ import numpy as np
 
 class AffinityPropagationCategorizer:
   """
-  TODO
+  This class uses the Affinity Propagation clustering algorithm to find the terms related 
+  to a given category.
   """
   
   def __init__(self, term_vectors):
+    """
+    Creates an instance of this class. The clustering is performed by this constructor, so 
+    the term vectors must be provided as argument.
+    Parameters
+    ----------
+    term_vectors : numpy array [number of terms, embedding dimension]
+      The embedding vectors of the terms to be categorized.
+    """
     self.term_vectors = term_vectors
     term_cosines = cosine_similarity(term_vectors)
     self.model = AffinityPropagation(affinity='precomputed', random_state=random.randint(0, 1000))
     self.model.fit(term_cosines)
   
   def number_of_clusters(self):
+    """
+    Returns
+    -------
+    int
+      The number of clusters found by the clustering algorithm.
+    """
     return self.model.cluster_centers_indices_.shape[0]
   
   def term_indices_for_category(self, category_vector):
+    """
+    Finds the terms related to a given category.
+    Parameters
+    ----------
+    category_vector : numpy array [embedding_dimension]
+      The embedding vector of the category.
+    Returns
+    -------
+    numpy array [number of related terms, embedding_dimension]
+      The indices of the terms related to a given category.
+    """
     similarities = cosine_similarity(category_vector.reshape(1, -1), self.term_vectors)
-    best_indices = np.argmax(similarities)
-    cluster_label = np.unique(self.model.labels_[best_indices])
-    if cluster_label.shape[0] > 1:
+    best_indices = np.argmax(similarities)                        # Finds the indices of the terms that are most similar to the category
+    cluster_label = np.unique(self.model.labels_[best_indices])   # Get the labels of the clusters which the most similar terms were assigned
+    if cluster_label.shape[0] > 1:                                # In an ideal case, the category is related to one cluster only. If this didn't happen, print a warning
       print('WARN [AffinityPropagationCategorizer]: I\'ve found more than one cluster for a category.')
     
-    return np.nonzero(self.model.labels_ == cluster_label[0])
+    return np.nonzero(self.model.labels_ == cluster_label[0])     # Return the indices of the terms in the cluster of the category
 
 # Test
 def test_AffinityPropagationCategorizer():
