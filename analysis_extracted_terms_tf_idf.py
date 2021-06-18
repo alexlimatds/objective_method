Updating ploting -analysis_extracted_terms_tf_idf.py: Corrected to use population data for tf-idf calculation; Ploting modified to consider all of the seed documents. -Updating artifact: analysis_extracted_terms_tf_idf.pdf.

analysis_extracted_terms_tf_idf.py
@@ -1,6 +1,7 @@
 """
 TF-IDF based analysis of the extracted terms.
 """
+from re import I
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 import seaborn as sns
@@ -116,32 +117,30 @@ class TfidfVectorizer:
 
 seed_corpus, seed_known_terms = data_readers.seed_set()
 population_corpus, population_known_terms = data_readers.population_set()
-#corpus = seed_corpus + population_corpus
-#known_terms = seed_known_terms + population_known_terms
-corpus = seed_corpus
-known_terms = seed_known_terms
+corpus = seed_corpus + population_corpus
+known_terms = seed_known_terms + population_known_terms
 
 extractor = terms_extraction.PosTagExtractor()
 pipe = TfidfVectorizer(extractor)
 pipe.fit(corpus, known_terms)
 
 n_seed_docs = len(seed_corpus)
-n_seed_keywords_lists = len(seed_known_terms) - seed_known_terms.count(None)
 tf_idf = pipe.transform(seed_corpus, seed_known_terms).toarray()
 v = pipe.vocabulary_list_
 
-_, axis = plt.subplots(n_seed_keywords_lists, 3, figsize=(14, 6 * n_seed_keywords_lists))
-plot_count = 0
+_, axis = plt.subplots(n_seed_docs, 3, figsize=(14, 6 * n_seed_docs))
 for i in range(n_seed_docs):
-    if seed_known_terms[i]: # the paper provides author's keywords
-        # histogram with tf-idf scores of all terms in the document
-        axis[plot_count,0].set_title(f'Histogram - document {i} - {np.count_nonzero(tf_idf[i])} terms')
-        min_positive = np.amin(tf_idf[i], where=(tf_idf[i] > 0), initial=1.0)
-        axis[plot_count,0].hist(tf_idf[i], range=(min_positive, tf_idf[i].max()))
-        axis[plot_count,0].grid(axis='y')
-        axis[plot_count,0].set_yscale('log')
-        axis[plot_count,0].set(xlabel='TF-IDF')
+    # histogram with tf-idf scores of all terms extracted from the document
+    axis[i,0].set_title(f'Histogram - document {i} - {np.count_nonzero(tf_idf[i])} terms')
+    min_positive = np.amin(tf_idf[i], where=(tf_idf[i] > 0), initial=1.0)
+    axis[i,0].hist(tf_idf[i], range=(min_positive, tf_idf[i].max()))
+    axis[i,0].grid(axis='y')
+    axis[i,0].set_yscale('log')
+    axis[i,0].set(xlabel='TF-IDF')
 
+    # TF-IDF scores of the author's keywords
+    axis[i,1].set_title("TF-IDF scores of authors' keywords")
+    if seed_known_terms[i]: # the paper provides author's keywords
         # tf-idf scores of the authors' keywords
         keywords = []
         key_scores = []
@@ -150,25 +149,24 @@ for i in range(n_seed_docs):
             keywords.append(k)
             key_scores.append(tf_idf[i,idx])
         y_pos = range(len(keywords))
-        axis[plot_count,1].barh(y_pos, key_scores)
-        axis[plot_count,1].grid(axis='x')
-        axis[plot_count,1].set_yticks(y_pos)
-        axis[plot_count,1].set_yticklabels(keywords)
-        axis[plot_count,1].set_title("TF-IDF scores of authors' keywords")
+        axis[i,1].barh(y_pos, key_scores)
+        axis[i,1].grid(axis='x')
+        axis[i,1].set_yticks(y_pos)
+        axis[i,1].set_yticklabels(keywords)
+    else:
+        axis[i,1].text(0.05, 0.5, "The paper doesn't provide\nauthors' keywords")
 
-        # top terms according to tf-idf score
-        n_top = 20
-        top_idx = np.argsort(tf_idf[i])[-1:-(n_top+1):-1]
-        terms = []
-        for idx in top_idx:
-            terms.append(v[idx])
-        y_pos = range(n_top)
-        axis[plot_count,2].barh(y_pos, tf_idf[i,top_idx])
-        axis[plot_count,2].grid(axis='x')
-        axis[plot_count,2].set_yticks(y_pos)
-        axis[plot_count,2].set_yticklabels(terms)
-        axis[plot_count,2].set_title(f"Top {n_top} TF-IDF scores")
-
-        plot_count += 1
+    # top terms according to tf-idf score
+    n_top = 20
+    top_idx = np.argsort(tf_idf[i])[-1:-(n_top+1):-1]
+    terms = []
+    for idx in top_idx:
+        terms.append(v[idx])
+    y_pos = range(n_top)
+    axis[i,2].barh(y_pos, tf_idf[i,top_idx])
+    axis[i,2].grid(axis='x')
+    axis[i,2].set_yticks(y_pos)
+    axis[i,2].set_yticklabels(terms)
+    axis[i,2].set_title(f"Top {n_top} TF-IDF scores")
 
 plt.savefig('analysis_extracted_terms_tf_idf.pdf', bbox_inches='tight')