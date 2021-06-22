91d929151a8931f590e33e5990b7dbdfeb8ac3ad
Alexandre Lima <alexlimatds@yahoo.com.br>
2021-06-22 11:44:24 -0300

    Removing files

--- experiment-01.py
+++
@@ -1,8 +0,0 @@
-import terms_pipeline
-import pandas as pd
-
-df = pd.read_csv('seed_set.csv')
-corpus = df['title'].str.cat(df[['abstract', 'keywords']], sep=' ', na_rep='').tolist()
-
-pipeline = terms_pipeline.TermsClusteringPipeline()
-pipeline.run(corpus, ['deep learning', 'legal domain'], 'experiment_01', log_clustering=True)
