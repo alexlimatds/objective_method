47c22c3a5bfa57322f006dc053a9237b67478c7b
Alexandre Lima <alexlimatds@yahoo.com.br>
2021-06-23 11:51:22 -0300

    Correcting bug in token based extraction
    -app-term_extraction-token.py: correcting the merge funciton.
    -Updating artifacts.

--- app-term_extraction-token.py
+++ app-term_extraction-token.py
@@ -2,9 +2,10 @@ from numpy import append
 import data_readers
 from terms_extractors import TokenExtractor, get_term_df
 
+# Function to merge the paper text and its provided keyterms
 def merge(text, known_terms):
   corpus = []
-  for i in range(len(seed_text)):
+  for i in range(len(text)):
     if known_terms[i]:
       corpus.append(text[i] + " " + ", ".join(known_terms[i]))
     else:
@@ -13,30 +14,33 @@ def merge(text, known_terms):
 
 extractor = TokenExtractor()
 
+# Extracting vocabulary
 seed_text, seed_known_terms = data_readers.seed_set()
 seed_corpus = merge(seed_text, seed_known_terms)
-terms, _ = extractor.extract_from_corpus(seed_corpus)
+tokens, _ = extractor.extract_from_corpus(seed_corpus)
 with open("extracted_terms-seed_set-token.csv", 'w') as csvfile:
-  csvfile.write(f'{len(terms)}\n')
-  for i in range(len(terms)):
-    csvfile.write(f'{terms[i]},{i}\n')
+  csvfile.write(f'{len(tokens)}\n')
+  for i in range(len(tokens)):
+    csvfile.write(f'{tokens[i]},{i}\n')
 
-terms_idx = {}
-for i, t in enumerate(terms):
-  terms_idx[t] = i
+tokens_idx = {}
+for i, t in enumerate(tokens):
+  tokens_idx[t] = i
 
-seed_occur_matrix = extractor.terms_by_doc(terms_idx, seed_corpus)
+# Extracting tokens from the seed set
+seed_occur_matrix = extractor.terms_by_doc(tokens_idx, seed_corpus)
 seed_term_df = get_term_df(seed_occur_matrix)
 with open("extracted_terms_df-seed_set-token.csv", 'w') as csvfile:
-  for i in range(len(terms)):
-    csvfile.write(f'{i},{terms[i]},{seed_term_df[i]}\n')
+  for i in range(len(tokens)):
+    csvfile.write(f'{i},{tokens[i]},{seed_term_df[i]}\n')
 
+# Extracting tokens from the population set
 population_text, population_known_terms = data_readers.population_set()
 population_corpus = merge(population_text, population_known_terms)
-population_occur_matrix = extractor.terms_by_doc(terms_idx, population_corpus)
+population_occur_matrix = extractor.terms_by_doc(tokens_idx, population_corpus)
 population_term_df = get_term_df(population_occur_matrix)
 with open("extracted_terms_df-population_set-token.csv", 'w') as csvfile:
-  for i in range(len(terms)):
-    csvfile.write(f'{i},{terms[i]},{population_term_df[i]}\n')
+  for i in range(len(tokens)):
+    csvfile.write(f'{i},{tokens[i]},{population_term_df[i]}\n')
 
-print(f'Done! Extracted {len(terms)} terms.')
+print(f'Done! Extracted {len(tokens)} terms.')
