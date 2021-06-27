fd42a745cdb5fa3c39d4b7b3e5315ea3335019dd
Alexandre Lima <alexlimatds@yahoo.com.br>
2021-06-27 11:06:43 -0300

    Adding code to persist the occurrence matrix
    -app-term_extraction-token.py:
      -merge function moved to data_readers.py;
      -adding the persistence of the occurrence matrix.
    -adding the occurrence_matrix-seed_set-token.csv file;
    -Updating artifacts.

--- app-term_extraction-token.py
+++ app-term_extraction-token.py
@@ -1,22 +1,12 @@
-from numpy import append
+import numpy as np
 import data_readers
 from terms_extractors import TokenExtractor, get_term_df
-
-# Function to merge the paper text and its provided keyterms
-def merge(text, known_terms):
-  corpus = []
-  for i in range(len(text)):
-    if known_terms[i]:
-      corpus.append(text[i] + " " + ", ".join(known_terms[i]))
-    else:
-      corpus.append(text[i])
-  return corpus
 
 extractor = TokenExtractor()
 
 # Extracting vocabulary
 seed_text, seed_known_terms = data_readers.seed_set()
-seed_corpus = merge(seed_text, seed_known_terms)
+seed_corpus = data_readers.merge_corpus_and_terms(seed_text, seed_known_terms)
 tokens, _ = extractor.extract_from_corpus(seed_corpus)
 with open("extracted_terms-seed_set-token.csv", 'w') as csvfile:
   csvfile.write(f'{len(tokens)}\n')
@@ -33,10 +23,11 @@ seed_term_df = get_term_df(seed_occur_matrix)
 with open("extracted_terms_df-seed_set-token.csv", 'w') as csvfile:
   for i in range(len(tokens)):
     csvfile.write(f'{i},{tokens[i]},{seed_term_df[i]}\n')
+np.savetxt("occurrence_matrix-seed_set-token.csv", seed_occur_matrix)
 
 # Extracting tokens from the population set
 population_text, population_known_terms = data_readers.population_set()
-population_corpus = merge(population_text, population_known_terms)
+population_corpus = data_readers.merge_corpus_and_terms(population_text, population_known_terms)
 population_occur_matrix = extractor.terms_by_doc(tokens_idx, population_corpus)
 population_term_df = get_term_df(population_occur_matrix)
 with open("extracted_terms_df-population_set-token.csv", 'w') as csvfile:
--- data_readers.py
+++ data_readers.py
@@ -2,6 +2,7 @@
 Functions to perform reading from the data files.
 """
 import pandas as pd
+import numpy as np
 
 def seed_set():
     """
@@ -50,3 +51,92 @@ def population_set():
             known_terms.append(None)
     
     return corpus, known_terms
+
+def merge_corpus_and_terms(texts, known_terms):
+    """
+    Merges a list of texts and a list of known terms. Both list must be the same 
+    number of items.
+    
+    Parameters
+    ----------
+    texts : list of string
+        The texts.
+    known_terms : list of list of string
+        The known terms for each text. Provide None when a text does not have known terms.
+    """
+    if len(texts) != len(known_terms):
+        raise ValueError("The provided lists must have the same number of items.")
+    corpus = []
+    for i in range(len(texts)):
+        if known_terms[i]:
+            corpus.append(texts[i] + " " + ", ".join(known_terms[i]))
+        else:
+            corpus.append(texts[i])
+    return corpus
+
+def read_vocabulary(file_name):
+    """
+    Returns the vocabulary from a CSV file holding the previously extrated terms. Its 
+    first line will be ignored. The remaining lines must have the following format: 
+    <term>,<identification index>
+
+    Parameters
+    ----------
+    file_name : string
+        The path of the file to be read
+    
+    Returns
+    -------
+    A dictionary whose each item is a term (string) index (int) pair.
+    """
+    vocab = {}
+    with open(file_name, 'r') as csvfile:
+        lines = csvfile.readlines()
+        skip = True # To skipe the first line
+        for l in lines:
+            if not skip:
+                line = l.split(sep=',')
+                vocab[line[0]] = int(line[1])
+            else:
+                skip = False
+    return vocab
+
+def read_df(file_name, vocabulary, n_docs=None):
+    """
+    Returns, from a CSV file, the document frequencies (DF) of terms in a vocabulary.
+    Each line from the file must have the following format: <term index>,<term>,<absolute df>
+    An excpetion is raised if the there is discrepancies between the term index from the 
+    vocabulary and the term index from the file.
+
+    Parameters
+    ----------
+    file_name : string
+        The path of the file to be read
+    vocabulary : a dictionary
+        A dictionary whose each item is a term (string) index (int) pair.
+    n_docs : float
+        If provided, the document frequencies values will be divided by this parameter. In other words, 
+        this function will return the relative document frequencies if this parameter is provided.
+    
+    Returns
+    -------
+    A list cotaining the read document frequencies. The list follows the 
+    vocabulary's term indexes.
+    """
+    v_len = len(vocabulary)
+    df = [None] * v_len
+    with open(file_name, 'r') as csvfile:
+        lines = csvfile.readlines()
+        for l in lines:
+            line = l.split(sep=',')
+            term = line[1]
+            idx = line[0]
+            if idx != vocabulary[term]:
+                raise RuntimeError(
+                    f"There is a discrepancy in the index of the |{term}| term. "
+                    "Vocabulary index: {vocabulary[term]}; File index: {idx}")
+            if n_docs:
+                df[idx] = float(line[2]) / n_docs
+            else:
+                df[idx] = float(line[2])
+    return df
